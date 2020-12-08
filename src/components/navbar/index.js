99be5c4f9352634251b2a9fb5ac61fc2574e7ff2
David <ddropping@gmail.com>
2020-12-08 14:38:59 -0800

    Create navbar

---
+++ src/components/navbar/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #cafff7;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const SortContainer = styled.div`
+  display: flex;
+`;
+
+export const SortItem = styled.div`
+  padding: 5px;
+  margin: 0 2px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  border-bottom: 2px solid transparent;
+  :hover {
+    border-bottom: 2px solid #00ad93;
+  }
+`;
+
+const index = () => {
+  return (
+    <Container>
+      <div>SortVisualizer</div>
+      <SortContainer>
+        <SortItem>Insertion Sort</SortItem>
+        <SortItem>Merge Sort</SortItem>
+        <SortItem>Quick Sort</SortItem>
+        <SortItem>Radix Sort</SortItem>
+      </SortContainer>
+    </Container>
+  );
+};
+
+export default index;
