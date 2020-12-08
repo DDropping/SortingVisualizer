6bec4d2a6da78dede3002289dfc743ef49b611db
David <ddropping@gmail.com>
2020-12-08 14:22:03 -0800

    Create generate array

--- src/components/mergeSort/index.js
+++ src/components/mergeSort/index.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const Container = styled.div``;
 
-const index = () => {
+const Index = ({ size }) => {
+  const [array, setArray] = useState([]);
+
+  const resetArray = () => {
+    setArray([]);
+    for (let i = 0; i < size; i++) {
+      array.push(Math.floor(Math.random() * (100 - 5 + 1) + 5));
+    }
+  };
+
+  useEffect(() => {
+    resetArray();
+    console.log(array);
+  }, []);
+
   return (
     <Container>
       <div>Merge sort</div>
@@ -11,4 +25,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Index;
