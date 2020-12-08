9b178f6d4bfe57cdb0032af28347caac5a5c4c57
David <ddropping@gmail.com>
2020-12-08 14:39:11 -0800

    Instal depenencies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Navbar from "../components/navbar";
 import MergeSort from "../components/mergeSort";
 
 const Home = () => {
+  const [arrayLength, setArrayLength] = useState(50);
+
   return (
     <div>
-      <div>Home</div>
-      <MergeSort />
+      <Navbar />
+      <MergeSort size={arrayLength} />
     </div>
   );
 };
